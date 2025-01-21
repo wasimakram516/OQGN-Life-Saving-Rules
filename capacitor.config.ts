07c5fc6b265feba7549037f6bf0b956e8c615803
Wasim Akram <mr.saim516@gmail.com>
2025-01-21 21:20:55 +0500

    feature/added apk file

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oqgnlsr.apk',
+  appName: 'OQGN LSR',
+  webDir: 'build'
+};
+
+export default config;
